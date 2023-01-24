35f903ef7a6baab10563e9152f0333ca23aa6cc5
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-01-24 14:56:45 +0000

    Se mostraron mas ejemplos de la función format con distintas operaciones basicas

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -1,9 +1,12 @@
 """ Programa1
     Nombre: Juan Carlos Hernandez Vazquez
     Fecha: 24/01/2023
-    Descripcion:En este programa observamos como funciona la funcion format """
+    Descripcion:En este programa observamos como funciona la funcion format de diferentes          maneras y diferenes operaciones aritmeticas"""
 
-variable1 = 10  #Variable para caadena de caracteres
-variable2 = 5  #Variable para caadena de caracteres
-print(variable1+variable2)  #Imprime la suma de dos variables
-print("{} + {} = {}".format(variable1,variable2,variable1+variable2))  #Imprime con la funcion format las variables y realiza la suma de estas 2 variables mostrando el resultado
+variable1 = 10  #  Variable para caadena de caracteres
+variable2 = 5  #  Variable para caadena de caracteres
+print(variable1+variable2)  #  Imprime la suma de dos variables
+print("{} + {} = {}".format(variable1,variable2,variable1+variable2))  #  Imprime con la funcion format las variables y realiza la suma de estas 2 variables mostrando el resultado
+print("{} - {} = {}".format(variable1,variable2,variable1-variable2))  #  Imprime con la funcion format la resta entre 2 variables
+print("{} = {} * {}".format(variable1*variable2,variable2,variable1))  #  Imprime cin la funcion format la multiplicacion de dos variables, las cuales se ordenaron de manera diferente
+print("el resultado de dividir {} / {} es {}".format(variable1/variable2,variable2,variable1))  #  Imprime con la funcion format la divicion de dos varables, se cambio la estructura para observar como tranaja la funcion
