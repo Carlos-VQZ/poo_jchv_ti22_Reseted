3ec3fcac5aa2e72fc18477039d8c64a9cecf154d
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-01-24 14:07:55 +0000

    Se genero programa.2 py en el cual se observó como funciona la función format, concatenando caracteres de diferentes tipos, en este caso un carácter sting y int

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |--|--|--|
 |1  |Hola mundo en python3|programa0.py
 |2  |Concatenación y comentarios multilinea  |programa1.py
+|3  |Concatenacion mediante funcion Format   |programa2.py
---
+++ semana2/programa2.py
@@ -0,0 +1,12 @@
+""" Programa1
+    Nombre: Juan Carlos Hernandez Vazquez
+    Fecha: 24/01/2023
+    Descripcion:
+    En este programa se realizo una concatenacion          mediante la funcion format la cual permite     
+    cincatenar variables de tipo string y variables de 
+    tipo int"""
+
+variable1 = "Hola " #Variable para caadena de caracteres
+variable2 = 619 #Variable para caadena de caracteres
+print("{}{}".format(variable1,variable2))#concatena caracteres e imprime con funcion format
+
