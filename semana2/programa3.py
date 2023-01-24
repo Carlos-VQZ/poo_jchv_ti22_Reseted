027730ef38fb0984a7a12e7356e2f754682c72d8
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-01-24 14:34:07 +0000

    Se creo programa.3.py en el cual se observo como funciona la funcion format imprimiendo y sumando 2 variables, se modifico el archivo README.md en el cual se agrego programa2.py y programa3.py

---
+++ semana2/programa3.py
@@ -0,0 +1,9 @@
+""" Programa1
+    Nombre: Juan Carlos Hernandez Vazquez
+    Fecha: 24/01/2023
+    Descripcion:En este programa observamos como funciona la funcion format """
+
+variable1 = 10  #Variable para caadena de caracteres
+variable2 = 5  #Variable para caadena de caracteres
+print(variable1+variable2)  #Imprime la suma de dos variables
+print("{} + {} = {}".format(variable1,variable2,variable1+variable2))  #Imprime con la funcion format las variables y realiza la suma de estas 2 variables mostrando el resultado
