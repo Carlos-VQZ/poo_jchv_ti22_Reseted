00eff8e4428eb58949d404ecdf7533cd296b5276
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-01-31 02:04:27 +0000

    SE MOSTRARON mas ejemplos de la función format con distintas operaciones básicas

--- semana2/programa2.py
+++ semana2/programa2.py
@@ -2,7 +2,7 @@
     Nombre: Juan Carlos Hernandez Vazquez
     Fecha: 24/01/2023
     Descripcion:
-    En este programa se realizo una concatenacion          mediante la funcion format la cual permite     
+    En este programa se realizo una concatenacion mediante la funcion format la cual permite     
     cincatenar variables de tipo string y variables de 
     tipo int"""
 
